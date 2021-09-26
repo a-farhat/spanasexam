e0de15aa99e274eef2091336e99699d793016483
Ahmad Farhat <akfarhat@gmail.com>
2021-09-26 11:19:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello")
+
+const domContainer = document.querySelector('#entryForm_container');
+ReactDOM.render(e(FormData), domContainer);
+
+function createSQL () {
+
+    let vExamSection = document.querySelector("txtExamSection").textContent;
+    let vNum = document.querySelector("txtNum").textContent;
+    let vQuestion = document.querySelector("txtQuestion").textContent;
+    let vA = document.querySelector("txtA").textContent;
+    let vB = document.querySelector("txtB").textContent;
+    let vC= document.querySelector("txtC").textContent;
+    let vD= document.querySelector("txtD").textContent;
+    let vAnswer = document.querySelector("txtAnswer").textContent;
+    
+    
+    let sqlString = ''
+
+}
