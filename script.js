9af4eea10e06d7418a3af32a91688db4bfcad375
a-farhat <akfarhat@gmail.com>
2021-09-27 07:54:46 +0300

    use getElementById instead of querySelector

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 
 const domContainer = document.querySelector('#entryForm_container');
-ReactDOM.render(e(FormData), domContainer);
+//ReactDOM.render(e(FormData), domContainer);
 
 function createSQL () {
 
-    let vExamSection = document.querySelector("txtExamSection").textContent;
-    let vNum = document.querySelector("txtNum").textContent;
-    let vQuestion = document.querySelector("txtQuestion").textContent;
-    let vA = document.querySelector("txtA").textContent;
-    let vB = document.querySelector("txtB").textContent;
-    let vC= document.querySelector("txtC").textContent;
-    let vD= document.querySelector("txtD").textContent;
-    let vAnswer = document.querySelector("txtAnswer").textContent;
+    let vQID = document.getElementById("txtQID").value;    
+    let vExamSection = document.getElementById("txtExamSection").value;
+    let vNum = document.getElementById("txtNum").value;
+    let vQuestion = document.getElementById("txtQuestion").value;
+    let vA = document.getElementById("txtA").value;
+    let vB = document.getElementById("txtB").value;
+    let vC= document.getElementById("txtC").value;
+    let vD= document.getElementById("txtD").value;
+    let vAnswer = document.getElementById("txtAnswer").value;
     
+
+    //INSERT [dbo].[ExamQuestions] ([QID], [ExamSection], [Num], [Question], [A], [B], [C], [D], [Answer]) VALUES (51, N'Comprehensive', 1, N'A patient with cataract would most commonly complain of which symptoms' ,N'Halos and rainbows around lights', N'Eye pain and irritation that worsens at night' ,N'Blurred and hazy vision', N'Eye strain and headache when doing close work' ,N'c')
     
-    let sqlString = ''
+    let sqlString = 'Insert into ExamQuestions ([QID], [ExamSection], [Num], [Question], [A], [B], [C], [D], [Answer]) VALUES('+vQID;
+    console.log(sqlString)
 
 }
